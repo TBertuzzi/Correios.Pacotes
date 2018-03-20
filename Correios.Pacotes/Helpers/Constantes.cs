f5834d7346604ddbd5acc9b7d69a0d429ea4be8e
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-03-20 15:52:55 -0300

    Finalizado pacote Nuget

--- Correios.Pacotes/Helpers/Constantes.cs
+++ Correios.Pacotes/Helpers/Constantes.cs
@@ -6,6 +6,6 @@ namespace Correios.Pacotes.Helpers
 {
     internal static class Constantes
     {
-        internal static string CorreioAPI = "http://sro.micropost.com.br/consulta.php";
+        internal static string CorreioAPI = "http://sro.micropost.com.br/consulta.php?objetos={0}";
     }
 }
